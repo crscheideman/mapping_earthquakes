e4d9eeed25968a66e57978fb1c2cfb8330a42f61
Christopher Scheideman <crscheideman@gmail.com>
2020-10-14 18:39:17 -0400

    updated code

--- Mapping_GeoJSON_Polygons/static/js/logic.js
+++ Mapping_GeoJSON_Polygons/static/js/logic.js
@@ -1,11 +1,11 @@
 // We create the tile layer that will be the background of our map.
-let night = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/navigation-preview-night-v4/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
 });
 // We create the dark view tile layer that will be an option for our map.
-let day = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/navigation-preview-day-v4/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v9/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
@@ -13,37 +13,38 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 
 // create a base layer that holds both maps
 let baseMaps = {
-    Night: night,
-    Day: day
+    "Streets": streets,
+    "Satellite Streets": satelliteStreets
 };
 
 // create the map object with a center and zoom level along with default layer.
 let map = L.map('mapid', {
-    center: [44.0, -80.0],
-    zoom: 2,
-    layers: [night]
+    center: [43.7, -79.3],
+    zoom: 11,
+    layers: [streets]
 });
 
 // pass our map layers into our layers control and add the layers control to the map
 L.control.layers(baseMaps).addTo(map);
 
-// accessing the Toronto airline routes GeoJSON URL
-let torontoData = "https://raw.githubusercontent.com/crscheideman/mapping_earthquakes/main/torontoRoutes.json";
+// accessing the Toronto neighborhoods GeoJSON URL
+let torontoHoods = "https://raw.githubusercontent.com/crscheideman/mapping_earthquakes/main/torontoNeighborhoods.json";
 
 // create a style for the lines
 let myStyle = {
-    color: "#ffffa1",
-    weight: 2
+    color: "blue",
+    fillColor: "yellow",
+    weight: 1
 }
 
 // grabbing our GeoJSON data
-d3.json(torontoData).then(function(data) {
+d3.json(torontoHoods).then(function(data) {
         console.log(data);
     // Creating a GeoJSON layer with the retrieved data.
     L.geoJson(data, {
         style: myStyle,
         onEachFeature: function(feature, layer) {
-            layer.bindPopup("<h3> Airline: " + feature.properties.airline + "</h3> <hr> <h3>Destination: " + feature.properties.dst + "</h3>");
+            layer.bindPopup("<h3> Neighborhood: " + feature.properties.AREA_NAME + "</h3>");
         }
     }).addTo(map);
 });
